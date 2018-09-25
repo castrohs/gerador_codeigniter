ae17712556c91652d659c1bc493f437d22a1e32a
Gustavo Castro <gustavocastrohs@gmail.com>
2018-09-25 17:35:13 -0300

    envia key

--- application/views/gerador.php
+++ application/views/gerador.php
@@ -94,7 +94,7 @@ foreach ($campos as $key => $campo) {
     
     echo $this->escreveview->escreve_btn("div_".$nome_controller."_vw") ;
     echo "<div id='div_".$nome_controller."_vw'>";
-     $echo = ($this->escrevecontroller->escreve_controller($nome_view,$nome_model) ) ;
+     $echo = ($this->escrevecontroller->escreve_controller($nome_view,$nome_model,$primary_key) ) ;
      highlight_string($echo);
     echo "</div>";
     
