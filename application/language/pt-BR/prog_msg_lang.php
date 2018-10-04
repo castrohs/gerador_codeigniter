1a004b2f1fc26d0f81721d3ffe7afa4fdde94c52
Gustavo Castro <gustavocastrohs@gmail.com>
2018-10-04 16:38:08 -0300

    corrigido bug no insert

--- application/language/pt-BR/prog_msg_lang.php
+++ application/language/pt-BR/prog_msg_lang.php
@@ -5,3 +5,5 @@ $lang['model_busca_um_array']="busca_por_array";
 $lang['model_busca_um']="busca_um";
 $lang['model_excluir']="excluir";
 $lang['controller_listar']="listar";
+$lang['model_cadastrar']="insert";
+$lang['controller_listar']="list";
